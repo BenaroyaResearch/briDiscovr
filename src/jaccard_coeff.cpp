fb7c154b967ca643658c9175b02a40360c8d1ab1
Mario Rosasco <mrosasco@benaroyaresearch.org>
2020-07-08 09:11:56 -0700

    turned comments from original source into docs

--- src/jaccard_coeff.cpp
+++ src/jaccard_coeff.cpp
@@ -1,18 +1,19 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-// Compute jaccard coefficient between nearest-neighbor sets
-//
-// Weights of both i->j and j->i are recorded if they have intersection. In this case
-// w(i->j) should be equal to w(j->i). In some case i->j has weights while j<-i has no
-// intersections, only w(i->j) is recorded. This is determinded in code `if(u>0)`.
-// In this way, the undirected graph is symmetrized by halfing the weight
-// in code `weights(r, 2) = u/(2.0*ncol - u)/2`.
-//
-// Author: Chen Hao, Date: 25/09/2015
-// Copied from https://github.com/JinmiaoChenLab/Rphenograph/blob/master/src/jaccard_coeff.cpp
-// for use in briDiscovr on 200611 under the Artistic-2.0 license
-
+//' Compute jaccard coefficient between nearest-neighbor sets
+//'
+//' Weights of both i->j and j->i are recorded if they have intersection. In this case
+//' w(i->j) should be equal to w(j->i). In some case i->j has weights while j<-i has no
+//' intersections, only w(i->j) is recorded. This is determinded in code `if(u>0)`.
+//' In this way, the undirected graph is symmetrized by halfing the weight
+//' in code `weights(r, 2) = u/(2.0*ncol - u)/2`.
+//'
+//' Author: Chen Hao, Date: 25/09/2015
+//' Copied from https://github.com/JinmiaoChenLab/Rphenograph/blob/master/src/jaccard_coeff.cpp
+//' for use in briDiscovr on 200611 under the Artistic-2.0 license
+//' @param idx NumericMatrix
+//' @return NumericMatrix
 //' @export
 // [[Rcpp::export]]
 NumericMatrix jaccard_coeff(NumericMatrix idx) {
