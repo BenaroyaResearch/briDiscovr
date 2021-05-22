97a05c089afe42d766accbdcf5e92ee6acbd8fcf
mjdufort <matt.dufort@gmail.com>
2021-05-22 00:08:59 -0700

    Fix bug with clusterDendro in metaclusteringPhase.R

--- R/metaclusteringPhase.R
+++ R/metaclusteringPhase.R
@@ -338,7 +338,7 @@ recutMetaclusters <- function(
 
   # cut the heatmap dendrogram to get phenotypic metaclusters
   colIndices <- cutree(
-    experiment$metaClusterDendro,
+    experiment$clusterDendro,
     k = nMetaclusters
   )
   # actual number of groups after cutting the tree
